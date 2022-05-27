93140ab5ff907426e390a2dafb2c31dbeebcc24d
村上信次 <smurakami@morisawa.co.jp>
2022-05-27 16:11:11 +0900

    install.shファイル追加

--- after_install.sh
+++ after_install.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-
-cd /home/ec2-user/front_react_back_node
-node index.js
-cd /home/ec2-user/front_react_back_node/manhour-app
-npm start
---
+++ restart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# cd /home/ec2-user/front_react_back_node/manhour-app
+# pm2 stop manhour-app
+# pm2 start "npm start" --name manhour-app -- start
+
+# cd /home/ec2-user/front_react_back_node/
+# pm2 stop index
+# pm2 start "node index.js" --name index -- start
